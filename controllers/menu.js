8e2e65767069dc8e8e1c37c0e61fc03f747b0a8b
Creeperkung <kawin.seangsuwan@gmail.com>
2025-04-17 00:52:13 +0700

    addMenuitem route + controller

--- controllers/menu.js
+++ controllers/menu.js
@@ -38,3 +38,22 @@ exports.getMenu = async (req, res, next) => {
         return res.status(500).json({ success: false, message: "Cannot find Menu" });
     }
 };
+exports.addMenuItem = async (req, res, next) => {
+    try {
+        req.body.restaurant = req.params.RestaurantId;
+        console.log(req.params);
+        const restaurant = await Restaurant.findById(req.params.RestaurantId);
+        if (!restaurant) {
+            return res.status(404).json({ success: false, message: `No restaurant with the id of ${req.params.RestaurantId}` });
+        }
+        const menuitem = await MenuItem.create(req.body);
+
+        res.status(200).json({
+            success: true,
+            data: menuitem
+        });
+    } catch (error) {
+        console.log(error.stack);
+        return res.status(500).json({ success: false, message: "Cannot create Menu" });
+    }
+};
--- routes/menu.js
+++ routes/menu.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {getMenus,getMenu} = require('../controllers/menu');
+const {getMenus,getMenu,addMenuItem} = require('../controllers/menu');
 
 const router = express.Router({mergeParams:true});
 
@@ -8,7 +8,7 @@ const router = express.Router({mergeParams:true});
 const {protect,authorize} = require('../middleware/auth');
 
 
-router.route('/').get(getMenus);
+router.route('/').get(getMenus).post(protect, authorize('admin','manager'),addMenuItem);
 router.route('/:id').get(getMenu);
 
 module.exports=router;
