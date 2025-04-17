7de866209deeb2cbea3f9daf7cab6d3908930704
Creeperkung <kawin.seangsuwan@gmail.com>
2025-04-17 23:28:07 +0700

    getOrders function&route

--- controllers/order.js
+++ controllers/order.js
@@ -3,7 +3,21 @@ const Reservation = require('../models/Reservation');
 
 
 exports.getOrders = async (req, res, next) =>{
+    let query;
+    console.log(req.params.reservationId)
+    query = Order.find({ reservation: req.params.reservationId }).populate('reservation')
+    try {
+        const order = await query;
 
+        res.status(200).json({
+            success: true,
+            count: order.length,
+            data: order
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Cannot get Menu" });
+    }
 }
 
 exports.addOrder = async (req, res, next) => {
--- routes/reservations.js
+++ routes/reservations.js
@@ -4,10 +4,10 @@ const {getReservations, getReservation, addReservation, updateReservation, delet
 
 const router = express.Router({mergeParams:true});
 
-
+const orderRouter = require('./order')
 const {protect,authorize} = require('../middleware/auth');
 
-
+router.use('/:reservationId/order/',orderRouter);
 router.route('/').get(protect, getReservations).post(protect, authorize('admin','user'),addReservation);
 router.route('/:id').get(protect,getReservation).put(protect, authorize('admin','user'),updateReservation).delete(protect, authorize('admin','user'),deleteReservation);
 
