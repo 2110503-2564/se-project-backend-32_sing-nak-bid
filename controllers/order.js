d3431c89765a0f0db7e9238048cb5946bc45303e
Kongphop Jitsongserm <kongphop.jitsongserm@gmail.com>
2025-04-17 22:32:44 +0700

    create addOrder

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,25 @@
 const Order = require("../models/OrderBooking")
+const Reservation = require('../models/Reservation');
+
 
 exports.getOrders = async (req, res, next) =>{
-    
+
 }
+
+exports.addOrder = async (req, res, next) => {
+    try {
+
+        req.body.reservation = req.params.Order.Reservation;
+
+        const order = await Order.create(req.body);
+
+        res.status(200).json({
+            success: true,
+            data: order
+        });
+
+    } catch (error) {
+        console.log(error.stack);
+        return res.status(500).json({ success: false, message: "Cannot create Order" });
+    }
+};
