8f440dcfe2fed4e62b4dd13471940978fb299151
tansit1 <tankhun006@gmail.com>
2025-04-26 00:35:12 +0700

    routing for ratings

--- controllers/rating.js
+++ controllers/rating.js
@@ -1,4 +1,4 @@
-const Rating = require("../model/Rating")
+const Rating = require("../models/Rating")
 const Reservation = require('../models/Reservation');
 const Restaurant = require('../models/Restaurant')
 
@@ -24,104 +24,104 @@ exports.getRatings = async (req, res, next) => {
 };
 
 
-//@desc Get all orders of the user
-//@route GET /api/v1/restaurant/:restaurantId/rating/:ratingId
-//@access  Private
-exports.getOrder = async (req, res, next) =>{
-    let query;
-    console.log(req.params.reservationId)
-    query = Order.findById(req.params.id).populate('reservation').populate('orderItems.menuItem').populate('restaurant');
-    try {
-        const order = await query;
+// //@desc Get all orders of the user
+// //@route GET /api/v1/restaurant/:restaurantId/rating/:ratingId
+// //@access  Private
+// exports.getOrder = async (req, res, next) =>{
+//     let query;
+//     console.log(req.params.reservationId)
+//     query = Order.findById(req.params.id).populate('reservation').populate('orderItems.menuItem').populate('restaurant');
+//     try {
+//         const order = await query;
 
-        res.status(200).json({
-            success: true,
-            data: order
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ success: false, message: "Cannot get Order" });
-    }
-}
+//         res.status(200).json({
+//             success: true,
+//             data: order
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ success: false, message: "Cannot get Order" });
+//     }
+// }
 
-//@desc Create an Order
-//@route POST /api/v1/reservations/:reservationId/order
-//@access  Private
-exports.addOrder = async (req, res, next) => {
-    try {
-        req.body.reservation = req.params.reservationId;
-        req.body.user = req.user.id;
-        console.log(req.params);
-        const reservation = await Reservation.findById(req.params.reservationId).populate('orderItems');
-        req.body.restaurant = reservation.restaurant;
-        if(!reservation){
-            return res.status(404).json({ success: false, message: `No Reservation with the id of ${req.params.reservationId}` });
-        }
-        if (reservation.orderItems.length >= 1 && req.user.role !== 'admin') {
-            return res.status(400).json({ success: false, message: `The user with ID ${req.user.id} has already made a order`});
-            }
-        const order = await Order.create(req.body);
-        res.status(200).json({
-            success: true,
-            data: order
-        });
+// //@desc Create an Order
+// //@route POST /api/v1/reservations/:reservationId/order
+// //@access  Private
+// exports.addOrder = async (req, res, next) => {
+//     try {
+//         req.body.reservation = req.params.reservationId;
+//         req.body.user = req.user.id;
+//         console.log(req.params);
+//         const reservation = await Reservation.findById(req.params.reservationId).populate('orderItems');
+//         req.body.restaurant = reservation.restaurant;
+//         if(!reservation){
+//             return res.status(404).json({ success: false, message: `No Reservation with the id of ${req.params.reservationId}` });
+//         }
+//         if (reservation.orderItems.length >= 1 && req.user.role !== 'admin') {
+//             return res.status(400).json({ success: false, message: `The user with ID ${req.user.id} has already made a order`});
+//             }
+//         const order = await Order.create(req.body);
+//         res.status(200).json({
+//             success: true,
+//             data: order
+//         });
 
-    } catch (error) {
-        console.log(error.stack);
-        return res.status(500).json({ success: false, message: "Cannot create Order" });
-    }
-};
+//     } catch (error) {
+//         console.log(error.stack);
+//         return res.status(500).json({ success: false, message: "Cannot create Order" });
+//     }
+// };
 
-//@desc Update an Order
-//@route PUT /api/v1/reservations/:reservationId/order/:orderId
-//@access  Private
-exports.updateOrder = async (req, res, next) => {
-    try {
-        let order =await Order.findById(req.params.id).populate('reservation')
-        if (!order) {
-            return res.status(404).json({ success: false, message: `No Order with the id of ${req.params.id}` });
-          }
-        if(order.reservation.user.toString()!== req.user.id && req.user.role === 'user'){
-            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this order`});
-        }
-        order = await Order.findByIdAndUpdate(req.params.id, req.body, {
-                new: true,
-                runValidators: true
-              });
-        res.status(200).json({
-            success: true,
-            data: order
-        });
+// //@desc Update an Order
+// //@route PUT /api/v1/reservations/:reservationId/order/:orderId
+// //@access  Private
+// exports.updateOrder = async (req, res, next) => {
+//     try {
+//         let order =await Order.findById(req.params.id).populate('reservation')
+//         if (!order) {
+//             return res.status(404).json({ success: false, message: `No Order with the id of ${req.params.id}` });
+//           }
+//         if(order.reservation.user.toString()!== req.user.id && req.user.role === 'user'){
+//             return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this order`});
+//         }
+//         order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+//                 new: true,
+//                 runValidators: true
+//               });
+//         res.status(200).json({
+//             success: true,
+//             data: order
+//         });
 
-    } catch (error) {
-        console.log(error.stack);
-        return res.status(500).json({ success: false, message: "Cannot update Order" });
-    }
-};
+//     } catch (error) {
+//         console.log(error.stack);
+//         return res.status(500).json({ success: false, message: "Cannot update Order" });
+//     }
+// };
 
 
-//@desc Delete an Order
-//@route DELETE /api/v1/reservations/:reservationId/order/:orderId
-//@access  Private
-exports.deleteOrder = async (req, res, next) => {
-    try {
-        const order = await Order.findById(req.params.id).populate('reservation');
-        console.log(order)
-        if (!order) {
-            return res.status(404).json({ success: false, message: `No order with the id of ${req.params.id}` });
-        }
-        if(order.reservation.user.toString()!== req.user.id && req.user.role === 'user'){
-            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to delete this order`});
-        }
+// //@desc Delete an Order
+// //@route DELETE /api/v1/reservations/:reservationId/order/:orderId
+// //@access  Private
+// exports.deleteOrder = async (req, res, next) => {
+//     try {
+//         const order = await Order.findById(req.params.id).populate('reservation');
+//         console.log(order)
+//         if (!order) {
+//             return res.status(404).json({ success: false, message: `No order with the id of ${req.params.id}` });
+//         }
+//         if(order.reservation.user.toString()!== req.user.id && req.user.role === 'user'){
+//             return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to delete this order`});
+//         }
 
-        await order.deleteOne();
-        res.status(200).json({
-            success: true,
-            data: {}
-        });
+//         await order.deleteOne();
+//         res.status(200).json({
+//             success: true,
+//             data: {}
+//         });
 
-    } catch (error) {
-        console.log(error.stack);
-        return res.status(500).json({ success: false, message: "Cannot delete Order" });
-    }
-};
+//     } catch (error) {
+//         console.log(error.stack);
+//         return res.status(500).json({ success: false, message: "Cannot delete Order" });
+//     }
+// };
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -7,11 +7,13 @@ const router = express.Router();
 //Include other resource routers
 const reservationRouter = require('./reservations');
 const menuRouter = require('./menu')
+const ratingRouter = require('./rating')
 const {protect,authorize} = require('../middleware/auth');
 
 //Re-route into other resource routers
 router.use('/:RestaurantId/reservations/',reservationRouter);
 router.use('/:RestaurantId/menu/',menuRouter);
+router.use('/:RestaurantId/rating',ratingRouter)
 router.route('/').get(getRestaurants).post(protect, authorize('admin'),createRestaurant);
 router.route('/user').get(getRestaurantsUser)
 router.route('/:id').get(getRestaurant).put(protect,authorize('admin'),updateRestaurant).delete(protect,authorize('admin'),deleteRestaurant);
