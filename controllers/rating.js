6d3ee82765b762ed5bf7ad8463c20cac1c5d7079
Creeperkung <kawin.seangsuwan@gmail.com>
2025-04-29 19:51:13 +0700

    fix delete rating

--- controllers/rating.js
+++ controllers/rating.js
@@ -123,28 +123,29 @@ exports.updateRating = async (req, res, next) => {
 //@route DELETE /api/v1/restaurants/:restuarantId/rating/:ratingId
 //@access  Private
 exports.deleteRating = async (req, res, next) => {
-    try {
-      const restaurant = await Restaurant.findById(req.params.RestaurantId);
-      if (!restaurant) {
-        return res.status(404).json({ success: false, message: 'Restaurant not found' });
-      }
-  
-      const rating = restaurant.ratings.id(req.params.ratingId);
-      if (!rating) {
-        return res.status(404).json({ success: false, message: 'Rating not found' });
-      }
-  
-      if (rating.user.toString() !== req.user.id && req.user.role === 'user') {
-        return res.status(401).json({ success: false, message: 'Not authorized to delete this rating' });
-      }
-  
-      rating.remove();
-      await restaurant.save();
-  
-      res.status(200).json({ success: true, data: {} });
-    } catch (error) {
-      console.error(error.stack);
-      return res.status(500).json({ success: false, message: "Cannot delete rating" });
+  try {
+    const restaurant = await Restaurant.findById(req.params.RestaurantId);
+    if (!restaurant) {
+      return res.status(404).json({ success: false, message: 'Restaurant not found' });
     }
-  };
+
+    const rating = restaurant.ratings.id(req.params.id);
+    if (!rating) {
+      return res.status(404).json({ success: false, message: 'Rating not found' });
+    }
+
+    if (rating.user.toString() !== req.user.id && req.user.role === 'user') {
+      return res.status(401).json({ success: false, message: 'Not authorized to delete this rating' });
+    }
+
+    restaurant.ratings.pull({ _id: req.params.id });
+    await restaurant.save();
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    console.error(error.stack);
+    return res.status(500).json({ success: false, message: "Cannot delete rating" });
+  }
+};
+
   
